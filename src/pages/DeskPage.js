98eb3651873aca73c04bcc59b38ba95dd8b8bb06
Esteban Canteros <estebanacanteros@gmail.com>
2021-09-13 10:03:06 -0300

    DeskPage design

--- src/pages/DeskPage.js
+++ src/pages/DeskPage.js
@@ -1,8 +1,52 @@
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons'
+import { Row, Col, Button, Divider, Typography } from 'antd'
+import ContentHeader from '../components/ContentHeader'
+
+const { Text } = Typography
+
 const DeskPage = () => {
+  const logout = () => {
+    console.log('logout')
+  }
+
+  const nextTicket = () => {
+    console.log('Next Ticket');
+  }
+
   return (
-    <div>
-      desk
-    </div>
+    <>
+      <Row>
+        <Col span={20}>
+          <ContentHeader title='Esteban' text="You're working on desk 5" />
+        </Col>
+        <Col span={4} align='right'>
+          <Button shape='round' type='danger' onClick={logout}>
+            <CloseCircleOutlined />
+            Logout
+          </Button>
+        </Col>
+      </Row>
+
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text>you are attending the number:</Text>
+          <Text style={{fontSize: 30}} type="danger"> 23 </Text>
+        </Col>
+      </Row>
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button
+            onClick={nextTicket} shape="round" type="primary"
+          > 
+            <RightOutlined />
+            Next
+          </Button>
+        </Col>
+      </Row>
+
+    </>
   )
 }
 
